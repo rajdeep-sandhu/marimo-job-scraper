3638523d95d3d5297a304f677fb3e6aa70e33639
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-21 11:11:26 +0100

    add fetch and parse methods with NotImplementedError

--- src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
+++ src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
@@ -13,3 +13,10 @@ class PythonJobsGithubScraper(JobScraper):
 
     def __init__(self: Self, base_url: str = "https://pythonjobs.github.io/"):
         super.__init__(base_url=base_url)
+    
+    def fetch(self: Self) -> requests.Response:
+        raise NotImplementedError
+
+    def parse(self: Self, raw_html: requests.Response) -> list[dict]:
+        """Parse html into a list of job dicts."""
+        raise NotImplementedError
