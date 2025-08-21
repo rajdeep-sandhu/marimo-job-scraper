d46308c3efde883254db1ce6a6980b483553d36a
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-21 11:25:31 +0100

    fix .__init__() to call super() correctly

--- src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
+++ src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
@@ -12,10 +12,10 @@ class PythonJobsGithubScraper(JobScraper):
     """Concrete scraper for https://pythonjobs.github.io/."""
 
     def __init__(self: Self, base_url: str = "https://pythonjobs.github.io/"):
-        super.__init__(base_url=base_url)
+        super().__init__(base_url=base_url)
     
     def fetch(self: Self) -> requests.Response:
-        raise NotImplementedError
+        pass
 
     def parse(self: Self, raw_html: requests.Response) -> list[dict]:
         """Parse html into a list of job dicts."""
