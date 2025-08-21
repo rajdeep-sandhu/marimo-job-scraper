6c150eb2871d5e868d38d1dc126d45136cb35bb9
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-21 11:07:46 +0100

    create python_jobs_github_scraper with class PythonJobsGithubScraper

--- src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
+++ src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
@@ -1,1 +1,15 @@
 # python_jobs_github_scraper.py
+from datetime import datetime
+from typing import Self
+
+import requests
+from bs4 import BeautifulSoup, ResultSet, Tag
+
+from marimo_job_scraper.scrapers.abstract_job_scraper import JobScraper
+
+
+class PythonJobsGithubScraper(JobScraper):
+    """Concrete scraper for https://pythonjobs.github.io/."""
+
+    def __init__(self: Self, base_url: str = "https://pythonjobs.github.io/"):
+        super.__init__(base_url=base_url)
