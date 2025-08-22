af122f8dda0a2e20919013e06095f90299443e6d
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-22 22:44:25 +0100

    add http adapter with retry strategy and error handling

--- src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
+++ src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
@@ -1,10 +1,12 @@
 # python_jobs_github_scraper.py
 from datetime import datetime
+from http import HTTPMethod, HTTPStatus
 from typing import Self
 
 import requests
 from bs4 import BeautifulSoup, ResultSet, Tag
 from requests import Response, Session
+from requests.adapters import HTTPAdapter, Retry
 
 from marimo_job_scraper.scrapers.abstract_job_scraper import JobScraper
 
@@ -16,11 +18,36 @@ class PythonJobsGithubScraper(JobScraper):
         """Constructor."""
         super().__init__(base_url=base_url)
 
-    def fetch(self: Self) -> Response:
+    def fetch(self: Self) -> Response | None:
         """Fetch response from url."""
-        response: Response = requests.get(url=self.base_url)
-        response.raise_for_status()
-        return response
+
+        # Define HTTPAdapter
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[
+                HTTPStatus.NOT_FOUND,
+                HTTPStatus.TOO_MANY_REQUESTS,
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                HTTPStatus.BAD_GATEWAY,
+                HTTPStatus.SERVICE_UNAVAILABLE,
+                HTTPStatus.GATEWAY_TIMEOUT,
+            ],
+            allowed_methods=[HTTPMethod.GET],
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+
+        # Get request
+        with Session() as session:
+            session.mount(prefix=self.base_url, adapter=adapter)
+            try:
+                response: Response = session.get(url=self.base_url, timeout=3.01)
+                response.raise_for_status()
+                return response
+            except requests.RequestException as err:
+                return None
+            finally:
+                session.close()
 
     def parse(self: Self, raw_html: Response) -> list[dict]:
         """Parse html into a list of job dicts."""
