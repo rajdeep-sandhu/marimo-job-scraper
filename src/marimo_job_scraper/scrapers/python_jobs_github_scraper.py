6bcfa41fbf5edda72f87ffd47546ba30501309d5
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-23 21:06:04 +0100

    amend .parse() to return list of parsed job dicts

--- src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
+++ src/marimo_job_scraper/scrapers/python_jobs_github_scraper.py
@@ -1,4 +1,5 @@
 # python_jobs_github_scraper.py
+import logging
 from datetime import datetime
 from http import HTTPMethod, HTTPStatus
 from typing import Self
@@ -55,10 +56,61 @@ class PythonJobsGithubScraper(JobScraper):
         if (not raw_html) or (raw_html) is None:
             return []
 
+        # Get job_cards from html
         soup: BeautifulSoup = BeautifulSoup(raw_html, features="html.parser")
         job_section: Tag = soup.find("section", class_="job_list")
         job_cards: ResultSet = job_section.find_all("div", class_="job")
 
+        # Parse job data into a list of dicts.
         jobs: list[dict] = []
 
+        for job_card in job_cards:
+            data_tags: list[str] = (
+                job_card.attrs.get("data-tags", "").strip().split(",")
+            )
+            link: str = f"{self.base_url[:-1]}{job_card.h1.a['href']}"
+            title: str = job_card.h1.a.text.strip()
+
+            # Parse info items
+            info_items: list[Tag] = job_card.find_all("span", class_="info")
+            info: dict = {item.i["class"][0]: item.text.strip() for item in info_items}
+
+            logger.debug(info)
+
+            # Set job_date to None if not in info or if not formatted as 'Tue, 23 Nov 2021'.
+            job_date: datetime.date = None
+            if info.get("i-calendar"):
+                try:
+                    job_date = datetime.strptime(
+                        info["i-calendar"].strip(), "%a, %d %b %Y"
+                    ).date()
+                except ValueError:
+                    job_date = None
+
+            detail: str = job_card.find("p", class_="detail").text.strip()
+
+            jobs.append(
+                {
+                    "title": title,
+                    "company": info.get("i-company", "").strip(),
+                    "location": info.get("i-globe", "").strip(),
+                    "tenure": info.get("i-chair", "").strip(),
+                    "date": job_date,
+                    "detail": detail,
+                    "data_tags": data_tags,
+                }
+            )
+
+            logger.debug(jobs)
+
         return jobs
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
+    )
+    logger.addHandler(handler)
