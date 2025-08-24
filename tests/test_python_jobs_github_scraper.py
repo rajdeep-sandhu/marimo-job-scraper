87bc85c4095232183a812ba386c7cf22cdeb0a39
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-24 20:44:27 +0100

    refactor to move test fixtures to conftest.py

--- tests/test_python_jobs_github_scraper.py
+++ tests/test_python_jobs_github_scraper.py
@@ -1,27 +1,7 @@
 # test_python_jobs_github_scraper.py
 
-from pathlib import Path
-
 import pytest
 from bs4 import BeautifulSoup, ResultSet, Tag
-
-from marimo_job_scraper.scrapers.python_jobs_github_scraper import (
-    PythonJobsGithubScraper,
-)
-
-
-@pytest.fixture
-def sample_html() -> str:
-    """Fixture to load the sample html file."""
-    filename = "pythonjobsgithub_sample.html"
-    filepath: Path = Path(__file__).parent.parent / "tests" / "fixtures" / filename
-    with filepath.open("r", encoding="utf-8") as file:
-        return file.read()
-
-
-@pytest.fixture
-def scraper() -> PythonJobsGithubScraper:
-    return PythonJobsGithubScraper()
 
 
 def test_fetch_returns_response(scraper):
