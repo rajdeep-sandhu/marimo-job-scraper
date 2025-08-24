c295a1ad6c7f87b184338f67d575c72143d1329a
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-24 14:23:55 +0100

    fix eror in provided pytest parameters

--- tests/test_httpclient.py
+++ tests/test_httpclient.py
@@ -9,3 +9,10 @@ def test_get_returns_response():
     response = HTTPClient.get(url)
     assert response.status_code == 200
     assert "application/json" in response.headers["Content-Type"]
+
+
+@pytest.mark.parametrize("status_code", [404, 500, 503])
+def test_get_returns_none_for_error_status(status_code):
+    url = "https://httpbin.org/status/{status_code}"
+    response = HTTPClient.get(url)
+    assert response is None
