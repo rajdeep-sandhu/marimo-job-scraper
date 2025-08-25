f9af704c110231b1786a291bf5755829137db48d
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-25 23:06:33 +0100

    amend sample_html() to simplify path to html file

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ from marimo_job_scraper.scrapers.python_jobs_github_scraper import (
 def sample_html() -> str:
     """Fixture to load the sample html file."""
     filename = "pythonjobsgithub_sample.html"
-    filepath: Path = Path(__file__).parent.parent / "tests" / "fixtures" / filename
+    filepath: Path = Path(__file__).parent / "fixtures" / filename
     with filepath.open("r", encoding="utf-8") as file:
         return file.read()
 
