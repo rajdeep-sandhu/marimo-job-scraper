253f6d5fc911377fcdb023e1549e0028ebd106bb
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-26 09:07:04 +0100

    create main.py marimo notebook to display scraped results

--- src/marimo_job_scraper/main.py
+++ src/marimo_job_scraper/main.py
@@ -8,13 +8,25 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
     from marimo_job_scraper.scrapers import JobScraper, PythonJobsGithubScraper
-    return (PythonJobsGithubScraper,)
+    return PythonJobsGithubScraper, mo
 
 
 @app.cell
 def _(PythonJobsGithubScraper):
     scraper = PythonJobsGithubScraper()
-    scraper.scrape()
+    jobs = scraper.scrape()
+    return (jobs,)
+
+
+@app.cell
+def _(jobs, mo):
+    table = mo.ui.table(jobs, selection='single', show_column_summaries='stats')
+    return (table,)
+
+
+@app.cell
+def _(mo, table):
+    mo.vstack([table, table.value])
     return
 
 
