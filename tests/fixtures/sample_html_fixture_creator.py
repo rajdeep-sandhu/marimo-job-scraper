1a95beabe295a6f0fd8decea4b70f03b7d35c827
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-08-27 08:41:39 +0100

    amend code formatting

--- tests/fixtures/sample_html_fixture_creator.py
+++ tests/fixtures/sample_html_fixture_creator.py
@@ -6,18 +6,22 @@ import requests
 from marimo_job_scraper.scrapers import JobScraper, PythonJobsGithubScraper
 from marimo_job_scraper.utils import HTTPClient
 
+
 def scrape_to_file(filepath: Path, scraper: JobScraper) -> None:
     if filepath.exists():
         print(f"{filepath.parts[-1]} already exists.")
         return None
-    
+
     response: requests.Response = scraper.fetch()
     filepath.write_text(data=response.text, encoding="utf-8")
     print(f"Created {filepath.parts[-1]}.")
     return None
 
+
 def main():
-    file_scrapers: dict = {"pythonjobsgithub_sample_download.html": PythonJobsGithubScraper}
+    file_scrapers: dict = {
+        "pythonjobsgithub_sample_download.html": PythonJobsGithubScraper
+    }
 
     for file, ScraperClass in file_scrapers.items():
         filepath = Path(__file__).parent / file
